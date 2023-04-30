e353165378fa5e401921f554fc21e3720b8e2c19
Denis Kuzmenok <kuzmenok.den@yandex.ru>
2023-04-30 23:32:15 +0700

    Lab 9 is already done

---
+++ PlaneTable/Models/TableElementDataBaseContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlaneTable.Models
+{
+    public partial class TableElementDataBaseContext : DbContext
+    {
+        public virtual DbSet<TableElement> Elements { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) => optionsBuilder.UseSqlite(@"Data Source=..\..\..\tabledata.db");
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ PlaneTable/ViewModels/ViewModelBase.cs
@@ -0,0 +1,9 @@
+﻿using ReactiveUI;
+
+namespace PlaneTable.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+
+    }
+}
